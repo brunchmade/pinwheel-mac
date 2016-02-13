ed1644c5a05a43f3391a2cdd29dee20694e128e9
Wells Riley <wells.riley@gmail.com>
2016-02-12 16:45:29 -0800

    shenanigans

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,9 +22,13 @@ String.prototype.toHHMMSS = function () {
     var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
     var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
     if (seconds < 10) {seconds = "0"+seconds;}
-    var time    = hours+':'+minutes+':'+seconds;
+
+    if (hours > 0) {
+      var time = hours+':'+minutes+':'+seconds;
+    } else {
+      var time = minutes+':'+seconds;
+    }
+
     return time;
 }
