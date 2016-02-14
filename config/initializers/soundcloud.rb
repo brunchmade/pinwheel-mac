239abac7af87001e5e557390dd24522f3cfe565b
Alex Baldwin <alexbaldwin@gmail.com>
2016-02-14 15:20:15 -0800

    SC env

--- config/initializers/soundcloud.rb
+++ config/initializers/soundcloud.rb
@@ -1,9 +1,9 @@
 module Soundcloud
   def self.client
     @client = Soundcloud.new(
-  :client_id     => 'b59d1f4b68bbc8f2b0064188f210117d',
-  :client_secret => 'bed0d37bd0eed984cb6d30bf4ee3a2e0',
-  :redirect_uri => 'http://localhost:5000/soundcloud/callback'
+      :client_id     => ENV['SOUNDCLOUD_ID'],
+      :client_secret => ENV['SOUNDCLOUD_SECRET'],
+      :redirect_uri =>  ENV['SOUNDCLOUD_REDIRECT']
     )
   end
 end
