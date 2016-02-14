4550be5982965b75524ed9c275b42295e1aa7763
Wells Riley <wells.riley@gmail.com>
2016-02-13 16:35:23 -0800

    styling fixes, fixed the electron app so it responds to keyboard input

--- electron-beta/main.js
+++ electron-beta/main.js
@@ -1,6 +1,144 @@
-const electron = require('electron');
-const app = electron.app;  // Module to control application life.
-const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var Menu = require('menu');
+
+var force_quit = false;
+
+var menu = Menu.buildFromTemplate([
+  {
+    label: 'Tumtable',
+    submenu: [
+      {
+        label: 'About Tumtable',
+        selector: 'orderFrontStandardAboutPanel:'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click: function() {mainWindow.reload();}
+      },
+      {
+        label: 'Hide Tumtable',
+        accelerator: 'CmdOrCtrl+H',
+        click: function() {mainWindow.hide();}
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Quit Tumtable',
+        accelerator: 'CmdOrCtrl+Q',
+        click: function() {force_quit=true; app.quit();}
+      },
+    ]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        label: 'Undo',
+        accelerator: 'CmdOrCtrl+Z',
+        role: 'undo'
+      },
+      {
+        label: 'Redo',
+        accelerator: 'Shift+CmdOrCtrl+Z',
+        role: 'redo'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Cut',
+        accelerator: 'CmdOrCtrl+X',
+        role: 'cut'
+      },
+      {
+        label: 'Copy',
+        accelerator: 'CmdOrCtrl+C',
+        role: 'copy'
+      },
+      {
+        label: 'Paste',
+        accelerator: 'CmdOrCtrl+V',
+        role: 'paste'
+      },
+      {
+        label: 'Select All',
+        accelerator: 'CmdOrCtrl+A',
+        role: 'selectall'
+      },
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      {
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click: function(item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.reload();
+        }
+      },
+      {
+        label: 'Toggle Full Screen',
+        accelerator: (function() {
+          if (process.platform == 'darwin')
+            return 'Ctrl+Command+F';
+          else
+            return 'F11';
+        })(),
+        click: function(item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+        }
+      },
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: (function() {
+          if (process.platform == 'darwin')
+            return 'Alt+Command+I';
+          else
+            return 'Ctrl+Shift+I';
+        })(),
+        click: function(item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.toggleDevTools();
+        }
+      },
+    ]
+  },
+  {
+    label: 'Window',
+    role: 'window',
+    submenu: [
+      {
+        label: 'Minimize',
+        accelerator: 'CmdOrCtrl+M',
+        role: 'minimize'
+      },
+      {
+        label: 'Close',
+        accelerator: 'CmdOrCtrl+W',
+        role: 'close'
+      },
+    ]
+  },
+  {
+    label: 'Help',
+    role: 'help',
+    submenu: [
+      {
+        label: 'Tumtable on GitHub',
+        click: function() { require('electron').shell.openExternal('https://github.com/brunchmade/tumtable') }
+      },
+    ]
+  }
+]);
 
 // Report crashes to our server.
 //electron.crashReporter.start();
@@ -21,8 +159,17 @@ app.on('window-all-closed', function() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
+  Menu.setApplicationMenu(menu);
+
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1000, height: 600, 'node-integration': false, 'title-bar-style': 'hidden-inset'});
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    minWidth: 700,
+    minHeight: 550,
+    'node-integration': false,
+    'title-bar-style': 'hidden-inset'
+  });
 
   // and load the index.html of the app.
   mainWindow.loadURL('http://localhost:3000');
@@ -36,14 +183,19 @@ app.on('ready', function() {
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
 
-  // Inject some useful CSS
-  mainWindow.show();
+  mainWindow.on('close', function(e){
+    if(!force_quit){
+      e.preventDefault();
+      mainWindow.hide();
+    }
+  });
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
+  mainWindow.on('closed', function(){
     mainWindow = null;
+    app.quit();
+  });
+
+  app.on('activate', function(){
+    mainWindow.show();
   });
 });
