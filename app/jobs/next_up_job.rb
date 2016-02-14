04018c8cd9be0e6d50319a4af6158f7950057126
Alex Baldwin <alexbaldwin@gmail.com>
2016-02-13 20:14:03 -0800

    Next up job is localized to the room you're in

--- app/jobs/next_up_job.rb
+++ app/jobs/next_up_job.rb
@@ -2,21 +2,21 @@ class NextUpJob < ApplicationJob
   include ApplicationHelper
   queue_as :default
 
-  def perform()
-    playing = Comment.where(now_playing: true, message_id: 1)
+  def perform(message)
+    playing = Comment.where(now_playing: true, message_id: message.id)
     playing.each do |c|
       c.now_playing = false
       c.save
     end
 
-    f = Comment.where(message_id: 1, now_playing: false, aired_at: nil).order(created_at: :desc).last
+    f = Comment.where(message_id: message.id, now_playing: false, aired_at: nil).order(created_at: :desc).last
     if f
       f.now_playing = true
       f.aired_at = Time.now.utc
       f.save
 
       next_track_at = Time.now.utc + (f.responses['duration'] / 1000)
-      NextUpJob.set(wait_until: next_track_at).perform_later
+      NextUpJob.set(wait_until: next_track_at).perform_later(message)
 
       push_track(f)
     end
