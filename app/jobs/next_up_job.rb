4249d86a7ef7bb8f7fd22bfdca47e9e7d902d953
Alex Baldwin <alexbaldwin@gmail.com>
2016-02-15 14:22:19 -0800

    wip next job

--- app/jobs/next_up_job.rb
+++ app/jobs/next_up_job.rb
@@ -2,26 +2,27 @@ class NextUpJob < ApplicationJob
   include ApplicationHelper
   queue_as :default
 
-  def perform(message_id)
+  def perform(message_id, comment_id)
+    comment = Comment.find(comment_id)
     message = Message.find(message_id)
-    playing = message.comments.where(now_playing: true)
+    playing = message.comments.where(now_playing: true).first
 
-    playing.each do |c|
-      c.now_playing = false
-      c.save
-    end
+    if comment.id == playing.id
+      playing.now_playing = false
+      playing.save
 
-    if f = message.comments.where(now_playing: false, aired_at: nil).order(created_at: :asc).first
-      now = Time.now.utc
+      if f = message.comments.where(now_playing: false, aired_at: nil).order(created_at: :asc).first
+        now = Time.now.utc
 
-      f.now_playing = true
-      f.aired_at = now
-      f.save
+        f.now_playing = true
+        f.aired_at = now
+        f.save
 
-      next_track_at = now + (f.responses['duration'] / 1000).ceil
-      NextUpJob.set(wait_until: next_track_at).perform_later(message.id)
+        next_track_at = now + (f.responses['duration'] / 1000).ceil
+        NextUpJob.set(wait_until: next_track_at).perform_later(message.id, f.id)
 
-      push_track(f)
+        push_track(f)
+      end
     end
   end
 end
